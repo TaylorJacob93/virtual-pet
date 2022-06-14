0a2bee8e90e402059bb73fd815d0500451819dc3
Jacob Taylor <jjakettaylor1993@hotmail.com>
2022-06-14 17:28:14 +0100

    deleting mistaken file

--- tests/Untitled-1.js
+++
@@ -1,6 +0,0 @@
-const Pet = require('./Pet.js'); 
-describe('Pet constructor', () => { 
-    it('returns an object', () => { 
-        expect(new Pet()).toBeInstanceOf(Object); 
-        }); 
-    });
