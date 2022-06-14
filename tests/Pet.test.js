2de7113387c90bb0f89820a609e94fff71055b96
Jacob Taylor <jjakettaylor1993@hotmail.com>
2022-06-14 18:16:58 +0100

    added growUp function with testing

--- tests/Pet.test.js
+++ tests/Pet.test.js
@@ -1,6 +1,29 @@
-const Pet = require('./Pet.js'); 
+const Pet = require('../virtual-pet/Pet.js');
+
+
 describe('Pet constructor', () => { 
     it('returns an object', () => { 
         expect(new Pet()).toBeInstanceOf(Object); 
         }); 
+    it('sets the name property', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.name).toEqual('Fido');
+      });
+      it('sets the initial age to 0', () => {
+        const pet = new Pet('Fido');
+    
+        expect(pet.age).toEqual(0);
+      });
     });
+
+describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
+    });
+  });
+
--- virtual-pet/Pet.js
+++ virtual-pet/Pet.js
@@ -1,6 +1,9 @@
 function Pet(name){
   this.name = name;
+  this.age = 0;
 }
-pet = new Pet('Fido');
+Pet.prototype.growUp = function() {
+  this.age += 1;
+};
 
 module.exports = Pet;
