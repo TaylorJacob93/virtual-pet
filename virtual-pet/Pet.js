e19af9d623fecb3e029e10abe73706c3a23b836e
Jacob Taylor <jjakettaylor1993@hotmail.com>
2022-06-19 19:50:30 +0100

    changed magic numbers to variables with values

--- virtual-pet/Pet.js
+++ virtual-pet/Pet.js
@@ -1,21 +1,32 @@
 const MAX_FITNESS = 10;
+const FITNESS_INCREMENT = 4;
+const FITNESS_DECREMENT = 3;
+const MIN_FITNESS = 0;
+const MAX_PREFITNESS_POINT = 6;
 const MIN_HUNGER = 0;
+const MAX_HUNGER = 10;
+const MAX_AGE = 30;
+const MIN_AGE = 0;
+const AGE_INCREMENET = 1;
+
+const HUNGER_INCREMENT = 5;
+
 
 function Pet(name){
   this.name = name;
-  this.age = 0;
-  this.hunger = 0;
-  this.fitness = 10;
+  this.age = MIN_AGE;
+  this.hunger = MIN_HUNGER;
+  this.fitness = MAX_FITNESS;
 }
 Pet.prototype = {
   get isAlive() {
-    return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+    return this.age < MAX_AGE && this.hunger < MAX_HUNGER && this.fitness > MIN_FITNESS;
   }
 };
 Pet.prototype.growUp = function() {
-  this.age += 1;
-  this.fitness -= 3;
-  this.hunger += 5;
+  this.age += AGE_INCREMENET;
+  this.fitness -= FITNESS_DECREMENT;
+  this.hunger += HUNGER_INCREMENT;
 
   if (!this.isAlive) {
     throw ('Your pet is no longer alive :(');
@@ -25,11 +36,11 @@ Pet.prototype.walk = function(){
   if (!this.isAlive) {
     throw ('Your pet is no longer alive :(');
   }
-  if(this.fitness >= 6){
+  if(this.fitness >= MAX_PREFITNESS_POINT){
     this.fitness = MAX_FITNESS;
   }
-  if(this.fitness > 0 && this.fitness < 6){
-    this.fitness += 4;
+  if(this.fitness > MIN_FITNESS && this.fitness < MAX_FITNESS){
+    this.fitness += FITNESS_INCREMENT;
   }
 }
 Pet.prototype.feed = function (){
